cc85a928df43874806b4ad5570fb0c952d5df2a3
Jason Piscatelli <JasonPiscatelli@Chriss-MBP.localdomain>
2018-08-07 18:09:49 -0400

    Build REST API

--- api/serialport/port.js
+++ api/serialport/port.js
@@ -1,31 +1,33 @@
 
-var SerialPort = require('serialport');// include the library
-//var portname = process.argv[2]; // get port name from the command line
-const portname = '/dev/ttyAMA0';
+var SerialPort = require('serialport');// include Serialport library
+
+const portname = '/dev/ttyAMA0';//Set port here 
 
 var myPort = new SerialPort(portname, {
-  autoOpen: false,
+  autoOpen: false, //turn auto port open off so we can do this with .open and .close method
   baudRate: 9600
-}); //Setup serial port
+}); //Setup serial port 
 
 var Readline = SerialPort.parsers.Readline; // make instance of Readline parser
 var parser = new Readline(); // make a new parser to read data
 
-module.exports ={ openPort: function(){
+//Exports for API to use
+module.exports ={ openPort: function(){//open port
   myPort.open();
   console.log("My PORT");
  },
 
-closePort: function(){
+closePort: function(){//close port
  myPort.close();
  },
 
-portWrite: function(s) {
+portWrite: function(s) {//send data to port
  myPort.write(s);
  }
 }
 
 myPort.pipe(parser); // pipe the serial stream to the parser
+
 //Callback functions for port and parser
 myPort.on('open', portOpen);
 parser.on('data', readSerialData);
@@ -39,8 +41,6 @@ function portOpen() {
 
 function readSerialData(data) {
   console.log(data);
-  setTimeout(portLoop, 2000);
-  //setTimeout(displayCommand(1), 2000);
 }
 
 function portClose() {
