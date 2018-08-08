6896c940fdd50c8f28de831561fbcab7e510e05f
Jason Piscatelli <JasonPiscatelli@Chriss-MBP.localdomain>
2018-08-08 09:42:58 -0400

    Fix output string of controlDigit to pad zeros .padStart(2,'0')

--- api/scoreboard/controlDigit.js
+++ api/scoreboard/controlDigit.js
@@ -14,13 +14,13 @@ module.exports = {
     var VAL = s.charCodeAt(0).toString(16);
     //console.log(SOH.toString(16)+DIGIT.toString(16)+ID0.toString(16)+ID1.toString(16)+VAL+CHK+ETX.toString(16));
     return (
-      SOH.toString(16) +
-      DIGIT.toString(16) +
-      ID0.toString(16) +
-      ID1.toString(16) +
-      VAL +
-      CHK +
-      ETX.toString(16)
+      SOH.toString(16).padStart(2, '0') +
+      DIGIT.toString(16).padStart(2, '0') +
+      ID0.toString(16).padStart(2, '0') +
+      ID1.toString(16).padStart(2, '0') +
+      VAL.padStart(2, '0') +
+      CHK.padStart(2, '0') +
+      ETX.toString(16).padStart(2, '0')
     );
   }
 };
