a3cdaeff560256f774e5202b245ce93e00da8ff1
Jason Piscatelli <JasonPiscatelli@Chriss-MBP.localdomain>
2018-08-09 23:46:17 -0400

    Build REST API

--- api/scoreboard/processAPI.js
+++ api/scoreboard/processAPI.js
@@ -1,92 +1,137 @@
 const buildCommand = require("./controlDigit");
 const com = require("../serialport/port");
-require ('../scoreboard/Global');
+require("../scoreboard/Global");
 
 module.exports = {
-    
-    
-    resetScoreboard: function() {
-      
-        homeDigitVal = 0;
-        awayDigitVal = 0;
-        inningDigitVal = 0;
-        ballsDigitVal = 0;
-        strikesDigitVal = 0;
+  resetScoreboard: function() {
+    homeDigitVal = 0;
+    awayDigitVal = 0;
+    inningDigitVal = 0;
+    ballsDigitVal = 0;
+    strikesDigitVal = 0;
+  },
 
-    },
-    
-    addHome: function() {
-       const max = 99; 
-       var val = '0';
+  addHome: function() {
+    const max = 99;
+    var val = "0";
 
-       if(homeDigitVal < max){
-        homeDigitVal++;
-        if(homeDigitVal<10){
-            var command = buildCommand.displayCommand(homeDigitID[0],val);
-            com.portWrite(command);
-            command = buildCommand.displayCommand(homeDigitID[1],homeDigitVal.toString());
-            com.portWrite(command);
-        }
-        else if(homeDigitVal>=10){
-            var val = homeDigitVal.toString().split('',2);
-            command = buildCommand.displayCommand(homeDigitID[0],val[0]);
-            com.portWrite(command);
-            command = buildCommand.displayCommand(homeDigitID[1],val[1]);
-            com.portWrite(val);
-        }
-       }
-      
-    },
-  
-    subtractHome: function() {
-        const min = 0; 
-        var val = '0';
- 
-        if(homeDigitVal > min){
-         homeDigitVal--;
-         if(homeDigitVal<10){
-             var command = buildCommand.displayCommand(homeDigitID[0],val);
-             com.portWrite(command);
-             command = buildCommand.displayCommand(homeDigitID[1],homeDigitVal.toString());
-             com.portWrite(command);
-         }
-         else if(homeDigitVal>=10){
-             var val = homeDigitVal.toString().split('',2);
-             command = buildCommand.displayCommand(homeDigitID[0],val[0]);
-             com.portWrite(command);
-             command = buildCommand.displayCommand(homeDigitID[1],val[1]);
-             com.portWrite(val);
-         }
-        }
-     },
+    if (homeDigitVal < max) {
+      homeDigitVal++;
+      if (homeDigitVal < 10) {
+        var command = buildCommand.displayCommand(homeDigitID[0], val);
+        com.portWrite(command);
+        command = buildCommand.displayCommand(
+          homeDigitID[1],
+          homeDigitVal.toString()
+        );
+        com.portWrite(command);
+      } else if (homeDigitVal >= 10) {
+        var val = homeDigitVal.toString().split("", 2);
+        command = buildCommand.displayCommand(homeDigitID[0], val[0]);
+        com.portWrite(command);
+        command = buildCommand.displayCommand(homeDigitID[1], val[1]);
+        com.portWrite(val);
+      }
+    }
+  },
 
-    addAway: function() {
-        const max = 99; 
-        var val = '0';
- 
-        if(awayDigitVal < max){
-         awayDigitVal++;
-         if(awayDigitVal<10){
-             var command = buildCommand.displayCommand(awayDigitID[0],val);
-             com.portWrite(command);
-             command = buildCommand.displayCommand(awayDigitID[1],awayDigitVal.toString());
-             com.portWrite(command);
-         }
-         else if(awayDigitVal>=10){
-             var val = awayDigitVal.toString().split('',2);
-             command = buildCommand.displayCommand(awayDigitID[0],val[0]);
-             com.portWrite(command);
-             command = buildCommand.displayCommand(awayDigitID[1],val[1]);
-             com.portWrite(val);
-         }
-        }
-     }
+  subtractHome: function() {
+    const min = 0;
+    var val = "0";
 
+    if (homeDigitVal > min) {
+      homeDigitVal--;
+      if (homeDigitVal < 10) {
+        var command = buildCommand.displayCommand(homeDigitID[0], val);
+        com.portWrite(command);
+        command = buildCommand.displayCommand(
+          homeDigitID[1],
+          homeDigitVal.toString()
+        );
+        com.portWrite(command);
+      } else if (homeDigitVal >= 10) {
+        var val = homeDigitVal.toString().split("", 2);
+        command = buildCommand.displayCommand(homeDigitID[0], val[0]);
+        com.portWrite(command);
+        command = buildCommand.displayCommand(homeDigitID[1], val[1]);
+        com.portWrite(val);
+      }
+    }
+  },
 
+  addAway: function() {
+    const max = 99;
+    var val = "0";
 
+    if (awayDigitVal < max) {
+      awayDigitVal++;
+      if (awayDigitVal < 10) {
+        var command = buildCommand.displayCommand(awayDigitID[0], val);
+        com.portWrite(command);
+        command = buildCommand.displayCommand(
+          awayDigitID[1],
+          awayDigitVal.toString()
+        );
+        com.portWrite(command);
+      } else if (awayDigitVal >= 10) {
+        var val = awayDigitVal.toString().split("", 2);
+        command = buildCommand.displayCommand(awayDigitID[0], val[0]);
+        com.portWrite(command);
+        command = buildCommand.displayCommand(awayDigitID[1], val[1]);
+        com.portWrite(val);
+      }
+    }
+  },
 
+  subtractAway: function() {
+    const min = 0;
+    var val = "0";
 
+    if (awayDigitVal > min) {
+      awayDigitVal--;
+      if (awayDigitVal < 10) {
+        var command = buildCommand.displayCommand(awayDigitID[0], val);
+        com.portWrite(command);
+        command = buildCommand.displayCommand(
+          awayDigitID[1],
+          awayDigitVal.toString()
+        );
+        com.portWrite(command);
+      } else if (awayDigitVal >= 10) {
+        var val = awayDigitVal.toString().split("", 2);
+        command = buildCommand.displayCommand(awayDigitID[0], val[0]);
+        com.portWrite(command);
+        command = buildCommand.displayCommand(awayDigitID[1], val[1]);
+        com.portWrite(val);
+      }
+    }
+  },
 
+  addInning: function() {
+    const max = 9;
+    var val = "0";
 
+    if (inningDigitVal < max) {
+      inningDigitVal++;
+      command = buildCommand.displayCommand(
+        inningDigitID,
+        inningDigitVal.toString()
+      );
+      com.portWrite(command);
+    }
+  },
 
+  subtractInning: function() {
+    const min = 0;
+    var val = "0";
+
+    if (inningDigitVal > min) {
+      inningDigitVal--;
+      command = buildCommand.displayCommand(
+        inningDigitID,
+        inningDigitVal.toString()
+      );
+      com.portWrite(command);
+    }
+  }
 };
--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,71 @@
 var express = require("express");
 var router = express.Router();
-require ("../api/scoreboard/Global")
-const request = require ("../api/scoreboard/processAPI")
+require("../api/scoreboard/Global");
+const request = require("../api/scoreboard/processAPI");
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Scoreboard US REST API" });
 });
 
+//add to home team score
 router.get("/addToHome", function(req, res, next) {
   request.addHome();
   res.status(200).json({
     message: "Success",
+    digit: homeDigitID,
     value: homeDigitVal
   });
 });
 
+//subtract from home team score
 router.get("/subtractFromHome", function(req, res, next) {
-  request.subtractHome();ç
+  request.subtractHome();
   res.status(200).json({
     message: "Success",
+    digit: homeDigitID,
     value: homeDigitVal
   });
 });
 
-
+//add to away team score
 router.get("/addToAway", function(req, res, next) {
   request.addAway();
   res.status(200).json({
     message: "Success",
+    digit: awayDigitID,
     value: awayDigitVal
   });
 });
 
+//subtract from away team score
+router.get("/subtractFromAway", function(req, res, next) {
+  request.subtractAway();
+  res.status(200).json({
+    message: "Success",
+    digit: awayDigitID,
+    value: awayDigitVal
+  });
+});
+
+//add to inning
+router.get("/addToInning", function(req, res, next) {
+  request.addInning();
+  res.status(200).json({
+    message: "Success",
+    digit: inningDigitID,
+    value: inningDigitVal
+  });
+});
+
+//subtract from inning
+router.get("/subtractFromInning", function(req, res, next) {
+  request.subtractInning();
+  res.status(200).json({
+    message: "Success",
+    digit: inningDigitID,
+    value: inningDigitVal
+  });
+});
 
 
 
