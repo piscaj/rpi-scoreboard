50391af6a2c77161ed2fedd77491a0c99aa698ed
Judge Dice <me@judgedice.com>
2018-08-09 21:56:43 -0400

    Not sure what I did

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require("express");
 var router = express.Router();
-
+require ("../api/scoreboard/Global")
+const request = require ("../api/scoreboard/processAPI")
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Scoreboard US REST API" });
 });
+
+router.get("/addToHome", function(req, res, next) {
+  request.addHome();
+  res.status(200).json({
+    message: "Success",
+    value: homeDigitVal
+  });
+});
+
+
+
+
 
 module.exports = router;
