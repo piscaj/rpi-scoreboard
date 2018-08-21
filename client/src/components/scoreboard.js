653bdea2db4455ffd7fd86a437b7451a7cba8ab1
Jason Piscatelli <JasonPiscatelli@Chriss-MBP.localdomain>
2018-08-20 21:18:10 -0400

    Update cleint code

--- client/src/components/scoreboard.js
+++ client/src/components/scoreboard.js
@@ -1,30 +1,52 @@
 import React, { Component } from "react";
-import { Button, Well } from 'react-bootstrap';
+import { Button, Well } from "react-bootstrap";
 
-class Scoreboard extends Component{
-  constructor(props){
+class Scoreboard extends Component {
+  constructor(props) {
     super(props);
     this.state = {
-      homeScore:0
-    }
+      homeScore: 0,
+      dataLoading: false,
+      error: null
+    };
   }
+
+  componentDidMount() {
+    fetch("/status/score")
+      .then(response => {
+        return response.json();
+      })
+      .then(data => this.setState({ homeScore: data.homeScore }));
+  }
+
   subtractHome = () => {
-    if(this.state.homeScore > 0){
-       this.setState({homeScore: this.state.homeScore -1 })
-    } else{
-      this.setState({homeScore: this.state.homeScore})
-    }
-  }
-  addHome = () => {this.setState({homeScore: this.state.homeScore +1 })}
+    fetch("/setDigit/home/subtract")
+      .then(response => {
+        return response.json();
+      })
+      .then(data => this.setState({ homeScore: data.homeScore }));
+  };
+
+  addHome = () => {
+    fetch("/setDigit/home/add")
+      .then(response => {
+        return response.json();
+      })
+      .then(data => this.setState({ homeScore: data.homeScore }));
+  };
   render() {
-    return(
+    return (
       <div>
         <div>
           <div>Home</div>
           <Well>
-          <Button bsStyle="primary" onClick={this.subtractHome}>-</Button>
-          <div>{this.state.homeScore}</div>
-          <Button bsStyle="primary" onClick={this.addHome}>+</Button>
+            <Button bsStyle="primary" onClick={this.subtractHome}>
+              -
+            </Button>
+            <div>{this.state.homeScore}</div>
+            <Button bsStyle="primary" onClick={this.addHome}>
+              +
+            </Button>
           </Well>
         </div>
       </div>
@@ -32,5 +54,3 @@ class Scoreboard extends Component{
   }
 }
 export default Scoreboard;
-
-
